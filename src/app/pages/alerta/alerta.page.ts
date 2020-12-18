2ef0e14c91259b4f04fc341282d20e89b5c91197
atimilson <atimilson95@gmail.com>
2020-12-18 17:17:31 -0300

    aula alert

--- src/app/pages/alerta/alerta.page.ts
+++ src/app/pages/alerta/alerta.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-alerta',
@@ -7,9 +9,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertaPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController : AlertController) { }
 
   ngOnInit() {
+    
   }
+  async exibealerta(){
+     const alert = await this.alertController.create({
+       header :'titulo alert',
+       subHeader : 'subTitulo aqui',
+       message : 'aqui o texto',
+       inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Placeholder 1'
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          value: 'hello',
+          placeholder: 'Placeholder 2'
+        },
+        {
+          name: 'name3',
+          value: 'http://ionicframework.com',
+          type: 'url',
+          placeholder: 'Favorite site ever'
+        },
+        // input date with min & max
+        {
+          name: 'name4',
+          type: 'date',
+          min: '2017-03-01',
+          max: '2018-01-12'
+        },
+        // input date without min nor max
+        {
+          name: 'name5',
+          type: 'date'
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          min: -5,
+          max: 10
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        },
+        {
+          name: 'name8',
+          type: 'password',
+          placeholder: 'Advanced Attributes',
+          cssClass: 'specialClass',
+          attributes: {
+            maxlength: 4,
+            inputmode: 'decimal'
+          }
+        }
+      ],
+       buttons: ['OK']
+     });
+     await alert.present();
+  }
+
 
 }
